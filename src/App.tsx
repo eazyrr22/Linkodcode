0c64efeebfd56c283f9cba78c4dd5fdc8ea31346
eazy <ezrirot@gmail.com>
2025-09-07 15:25:42 +0300

    posts page component receiving data as json obj and loading it to a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Post from './components/Post'
+import PostsPage from './pages/PostsPage'
 
 export default function App() {
   return (
     <div>
+      <PostsPage filePath = '' ></PostsPage>
       <Post/>
     </div>
   )
