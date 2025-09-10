caf93ae8cf18976f323de49e1aa1b902ed14e923
eazy <ezrirot@gmail.com>
2025-09-10 15:31:50 +0300

    load posts error fixed

--- frontEnd/src/pages/PostsPage.tsx
+++ frontEnd/src/pages/PostsPage.tsx
@@ -2,9 +2,9 @@ import PostCard from "../components/PostCard";
 import type { Post } from "../itemType";
 import { useId } from "react";
 
-export default function PostsPage({ data }: Post[]) {                           // TODO: set the data type
-  
-  const postsArr = [data].map((post: Post) => (
+export default function PostsPage({ data }: Post[]) {
+  // TODO: set the data type
+  const postsArr = data.map((post: Post) => (
     <li key={useId()} className="post-list-item">
       <PostCard
         imgUrl={post.imgUrl}
