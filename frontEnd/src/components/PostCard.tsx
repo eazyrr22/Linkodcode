fd594df7bfd200f2f421d355bddfd88d9fc74af1
eazy <ezrirot@gmail.com>
2025-09-10 17:25:29 +0300

    post card style

--- frontEnd/src/components/PostCard.tsx
+++ frontEnd/src/components/PostCard.tsx
@@ -5,15 +5,14 @@ import type { Post } from "../itemType";
 export default function PostCard(post: Post) {
   return (
     <div className="post-card">
-      <h2>Card Title</h2>
       <div className="post-head">
-        <div className="post-head-item">
+        <div className="post-head-item-left">
           <img src={post.imgUrl} alt="post img" />
           <h3 className="">{post.authorName}</h3>
         </div>
-        <div className="post-head-item">
+        <div className="post-head-item-right">
           <img src="src/assets/like-icon.png" alt="" />
-          <h4>{Date().toString()}</h4>
+          <h4>{new Date().toLocaleDateString()}</h4>
         </div>
       </div>
       <p className="post-main-article">post description{post.description}</p>
