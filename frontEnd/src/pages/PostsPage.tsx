05dad41a6622e1b36fab917f087abeca91cfd4ed
eazy <ezrirot@gmail.com>
2025-09-10 15:01:57 +0300

    posts fetch debug

--- frontEnd/src/pages/PostsPage.tsx
+++ frontEnd/src/pages/PostsPage.tsx
@@ -2,22 +2,16 @@ import PostCard from "../components/PostCard";
 import type { Post } from "../itemType";
 import { useId } from "react";
 
-export default function PostsPage({ data }:Post[]) {       // set the data type
+export default function PostsPage({ data }: Post[]) {                           // TODO: set the data type
   
-  return (
-    <div>
-      <ul>
-        {[data].map((post: Post) => (
-            <li key={useId()}>
-              <PostCard
-                imgUrl={post.imgUrl}
-                description={post.description}
-                authorName={post.authorName}
-              ></PostCard>
-            </li>
-          )
-        )}
-      </ul>
-    </div>
-  );
+  const postsArr = [data].map((post: Post) => (
+    <li key={useId()} className="post-list-item">
+      <PostCard
+        imgUrl={post.imgUrl}
+        description={post.description}
+        authorName={post.authorName}
+      ></PostCard>
+    </li>
+  ));
+  return <ul id="posts-list">{postsArr}</ul>;
 }
